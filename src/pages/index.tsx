ed3082594f0a22b25e988887a4395cbf46d644f7
Andre <andre.fabian.melo@gmail.com>
2021-04-02 12:23:05 -0300

    Add stripe payment system

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+type HomeProps = {
+  product: {
+    priceId: string
+    amount: string
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  
   return (
     <>
       <Head>
@@ -17,13 +27,34 @@ export default function Home() {
 
           <p>
             Get access to all publications <br/>
-            <span>for $9.99/month</span>
+            <span>for {product.amount} /month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="images/avatar.svg" alt="girl coding"/>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IbWnjC61N20BTfFqSehlkhs', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format((price.unit_amount / 100))
+  }
+
+  return {
+    revalidate: 60 * 60 * 24, //24 hours
+    props: {
+      product,
+    }
+  }
 }
